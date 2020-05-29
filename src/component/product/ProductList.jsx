ffbefde30c0115bc49627895a8ff19c1f25741bd
Pinot. Kim <pinot.kim@kakao.com>
2020-05-29 11:21:49 +0900

    :construction: Add multi table function
    WIP : infinite scroll

--- src/component/product/ProductList.jsx
+++ src/component/product/ProductList.jsx
@@ -8,7 +8,7 @@ import ProductItem from './ProductItem';
 const ProductList = (props) => {
   const [productList, setProductList] = useState([]);
 
-  const { html, onPushTableLength } = props;
+  const { htmls, onPushTableLength } = props;
 
   const handlePushTableLength = (e = 0) => {
     onPushTableLength(e);
@@ -16,118 +16,123 @@ const ProductList = (props) => {
 
   useEffect(() => {
     const table = [];
-    const tableHTML = parseHTML(html)
-      .getElementsByClassName('Ltbl_list')[0];
 
-    if (!tableHTML) {
+    if (!htmls.length) {
       setProductList([]);
       handlePushTableLength(-1);
       return;
     }
 
-    if (tableHTML.rows[1].cells[0].innerText === '검색결과가 없습니다.') {
+    if (htmls[0].match(/검색결과가 없습니다./g)) {
       setProductList([]);
       handlePushTableLength(0);
       return;
     }
 
-    // get table
-    for (let i = 1; i < tableHTML.rows.length; i += 1) {
-      let rowTemp = '';
+    const tableLength = parseHTML(htmls[0])
+      .getElementsByClassName('txtblue')[0]
+      .innerText
+      .match(/[0-9]{1,10}/g)[0];
 
-      // row에 데이터 추가하기
-      for (let j = 0; j < tableHTML.rows[i].cells.length; j += 1) {
-        rowTemp += tableHTML.rows[i].cells[j].innerHTML;
-      }
+    handlePushTableLength(tableLength);
 
-      rowTemp = rowTemp
-        .replace(/[\t\n]/g, '')
-        .replace(/[ ]{2}/g, ' ');
+    const phone = new RegExp(/(0[0-9]{1,2})-([0-9]{3,4})-([0-9]{4})/g);
+    const agency = new RegExp(/([가-힣]{2,4}(지방법원|지원))/g);
+    const date = new RegExp(/(20[1-3][0-9])\.([01][0-9])\.([0-3][0-9])/g);
+    const uniqueKey = new RegExp(/(20[0-3][0-9])([0-9]{10,13})/g);
+    const detailFunc = new RegExp(/detailSrchForDongsan\('([가-힣]{2,4}(지방법원|지원))', '(20[0-3][0-9])([0-9]{10,13})', '[0-9]{1,3}'\)/g);
 
-      const isMulti = !!rowTemp.match(/<br>\(경합\)/g);
-      const phone = new RegExp(/(0[0-9]{1,2})-([0-9]{3,4})-([0-9]{4})/g);
-      const agency = new RegExp(/([가-힣]{2,4}(지방법원|지원))/g);
-      const date = new RegExp(/(20[1-3][0-9])\.([01][0-9])\.([0-3][0-9])/g);
-      const uniqueKey = new RegExp(/(20[0-3][0-9])([0-9]{10,13})/g);
-      const detailFunc = new RegExp(/detailSrchForDongsan\('([가-힣]{2,4}(지방법원|지원))', '(20[0-3][0-9])([0-9]{10,13})', '[0-9]{1,3}'\)/g);
+    htmls.map((html) => {
+      const tableHTML = parseHTML(html)
+        .getElementsByClassName('Ltbl_list')[0];
 
-      const getNotice = (text) => (!text ? null : text[0]
-        .replace(/showBigoInfo\('/g, '')
-        .replace(/', event\)/g, ''));
+      // get table
+      for (let i = 1; i < tableHTML.rows.length; i += 1) {
+        let rowTemp = '';
 
-      console.log(isMulti);
+        // row에 데이터 추가하기
+        for (let j = 0; j < tableHTML.rows[i].cells.length; j += 1) {
+          rowTemp += tableHTML.rows[i].cells[j].innerHTML;
+        }
 
-      const getIncidentNum = (text) => (isMulti
-        ? text
-          .match(/<a[a-zA-Z가-힣0-9="'#,;() ]*>([가-힣a-z0-9 <>()/]*)\(경합\)/g)[0]
-          .replace(/<a[a-zA-Z가-힣0-9="'#,;() ]*>/g, '')
-          .replace(/<br><b>/g, ' ')
-          .replace(/<\/b><\/a>/g, '')
-          .replace(/<br>\(경합\)/g, ' (경합)')
-          .replace(/<br>/g, ' 및 ')
-        : text
-          .match(/([가-힣]{2,4}(지방법원|지원))(<br><b>)((20[1-3][0-9])[본징가][0-9]{1,5})/g)[0]
-          .replace(/<br><b>/g, ' '));
+        rowTemp = rowTemp
+          .replace(/[\t\n]/g, '')
+          .replace(/[ ]{2}/g, ' ');
 
-      console.log(rowTemp);
+        const getNotice = (text) => (!text ? null : text[0]
+          .replace(/showBigoInfo\('/g, '')
+          .replace(/', event\)/g, ''));
 
-      const row = {
-        incidentNumber: getIncidentNum(rowTemp),
-        link: {
-          depart: rowTemp
-            .match(detailFunc)[0]
-            .match(agency)[0],
-          uniqueKey: Number(rowTemp
-            .match(detailFunc)[0]
-            .match(uniqueKey)[0]),
-          objectNum: Number(rowTemp
-            .match(detailFunc)[0]
-            .match(/'[0-9]{1,3}'/g)[0]
-            .replace(/^'|'$/g, '')),
-        },
-        place: {
-          address: rowTemp
-            .match(/<a[a-zA-Z가-힣0-9="'#,;() ]*>[a-zA-Z가-힣0-9\-,() ]*<\/a>/g)[0]
+        const isMulti = !!rowTemp.match(/<br>\(경합\)/g);
+
+        const getIncidentNum = (text) => (isMulti
+          ? text
+            .match(/<a[a-zA-Z가-힣0-9="'#,;() ]*>([가-힣a-z0-9 <>()/]*)\(경합\)/g)[0]
             .replace(/<a[a-zA-Z가-힣0-9="'#,;() ]*>/g, '')
-            .replace(/<\/a>/g, '')
-            .replace(/,/g, ', '),
-          type: rowTemp.match(/(공장|동식물사육재배장|소매점|도매점|가정집|사무실|서비스제공시설|보관시설)|(물류|지방법원|지법원)/g)[0],
-        },
-        product: {
-          info: rowTemp
-            .match(/\[[a-zA-Z가-힣0-9 ,+\-=()]*]/g)[0]
-            .replace(/^\[|]$/g, '')
-            .replace(/[0-9,]*원/g, ''),
-          saleDate: rowTemp.match(date)[0],
-        },
-        depart: {
-          name: `${rowTemp.match(agency)[0]} ${rowTemp.match(/[0-9]부/g)[0]}`,
-          phone: rowTemp
-            .replace(/[)]/g, '-')
-            .match(phone)[0],
-        },
-        cost: {
-          appraisal: Number(rowTemp
-            .match(/<div class="tbl_btm_(no|)line">[0-9,]*<\/div>/g)[0]
-            .replace(/<div class="tbl_btm_(no|)line">/g, '')
-            .replace(/<\/div>/g, '')
-            .replace(/[,]/g, '')),
-          lowestBid: Number(rowTemp
-            .match(/<div class="tbl_btm_(no|)line">[0-9,]*<\/div>/g)[0]
-            .replace(/<div class="tbl_btm_(no|)line">/g, '')
-            .replace(/<\/div>/g, '')
-            .replace(/[,]/g, '')),
-        },
-        notice: getNotice(rowTemp.match(/showBigoInfo\('[가-힣0-9 .:-]*', event\)/g)),
-      };
+            .replace(/<br><b>/g, ' ')
+            .replace(/<\/b><\/a>/g, '')
+            .replace(/<br>\(경합\)/g, ' (경합)')
+            .replace(/<br>/g, ' 및 ')
+          : text
+            .match(/([가-힣]{2,4}(지방법원|지원))(<br><b>)((20[1-3][0-9])[본징가][0-9]{1,5})/g)[0]
+            .replace(/<br><b>/g, ' '));
 
-      table.push(row);
-    }
+        const row = {
+          incidentNumber: getIncidentNum(rowTemp),
+          link: {
+            depart: rowTemp
+              .match(detailFunc)[0]
+              .match(agency)[0],
+            uniqueKey: Number(rowTemp
+              .match(detailFunc)[0]
+              .match(uniqueKey)[0]),
+            objectNum: Number(rowTemp
+              .match(detailFunc)[0]
+              .match(/'[0-9]{1,3}'/g)[0]
+              .replace(/^'|'$/g, '')),
+          },
+          place: {
+            address: rowTemp
+              .match(/<a[a-zA-Z가-힣0-9="'#,;() ]*>[a-zA-Z가-힣0-9\-,() ]*<\/a>/g)[0]
+              .replace(/<a[a-zA-Z가-힣0-9="'#,;() ]*>/g, '')
+              .replace(/<\/a>/g, '')
+              .replace(/,/g, ', '),
+            type: rowTemp.match(/(공장|동식물사육재배장|소매점|도매점|가정집|사무실|서비스제공시설|보관시설)|(물류|지방법원|지법원)/g)[0],
+          },
+          product: {
+            info: rowTemp
+              .match(/\[[a-zA-Z가-힣0-9 ,+\-=()/:]*]/g)[0]
+              .replace(/^\[|]$/g, '')
+              .replace(/[0-9,]*원/g, ''),
+            saleDate: rowTemp.match(date)[0],
+          },
+          depart: {
+            name: `${rowTemp.match(agency)[0]} ${rowTemp.match(/[0-9]부/g)[0]}`,
+            phone: rowTemp
+              .replace(/[)]/g, '-')
+              .match(phone)[0],
+          },
+          cost: {
+            appraisal: Number(rowTemp
+              .match(/<div class="tbl_btm_(no|)line">[0-9,]*<\/div>/g)[0]
+              .replace(/<div class="tbl_btm_(no|)line">/g, '')
+              .replace(/<\/div>/g, '')
+              .replace(/[,]/g, '')),
+            lowestBid: Number(rowTemp
+              .match(/<div class="tbl_btm_(no|)line">[0-9,]*<\/div>/g)[0]
+              .replace(/<div class="tbl_btm_(no|)line">/g, '')
+              .replace(/<\/div>/g, '')
+              .replace(/[,]/g, '')),
+          },
+          notice: getNotice(rowTemp.match(/showBigoInfo\('[가-힣0-9 .:-]*', event\)/g)),
+        };
 
+        table.push(row);
+      }
+    });
     setProductList(table);
-    handlePushTableLength(table.length);
     // eslint-disable-next-line
-  }, [html]);
+  }, [htmls]);
 
   return (
     <Container fluid>
--- src/page/ProductListPage.jsx
+++ src/page/ProductListPage.jsx
@@ -12,14 +12,22 @@ const ProductListPage = () => {
   const [productListPageState, setProductListPageState] = useState({
     state: 'info',
     text: '요청 대기중',
-    data: '',
+    data: [],
+    isOverflow: false,
   });
+
+  console.log(productListPageState);
 
   const pushTableLength = (e) => {
     setTableLength(e);
+
+    setProductListPageState({
+      ...productListPageState,
+      isOverflow: e > 40,
+    });
   };
 
-  const frmData = `_NEXT_SRNID=${process.env.REACT_APP_SRN_ID}&termStartDt=${moment().format('YYYY.MM.DD')}&termEndDt=${moment().add(2, 'months').format('YYYY.MM.DD')}&maeMokmulNm=${encodeText(clearSpace(searchedText))}`;
+  const frmData = `_NEXT_SRNID=${process.env.REACT_APP_SRN_ID}&termStartDt=${moment().format('YYYY.MM.DD')}&termEndDt=${moment().add(2, 'months').format('YYYY.MM.DD')}&maeMokmulNm=${encodeText(clearSpace(searchedText))}&page=default40&targetRow=${1}`;
 
   useEffect(() => {
     Axios.post(process.env.REACT_APP_PRODUCT_LIST_ENDPOINT, frmData, {})
@@ -27,29 +35,31 @@ const ProductListPage = () => {
         if (data.match(/"해당 IP는 비정상적인 접속으로 보안정책에 의하여 차단되었습니다."/g)) {
           console.error('해당 IP는 비정상적인 접속으로 서버 보안정책에 의하여 차단되었습니다.');
           setProductListPageState({
+            ...productListPageState,
             state: 'danger',
             text: '오류 : 해당 IP가 차단되었습니다.',
-            data: '',
           });
         } else if (data.match(/물건상세검색 결과/g)) {
           console.log('요청 성공 😭');
           setProductListPageState({
+            ...productListPageState,
             state: 'success',
             text: '요청 성공',
-            data,
+            data: [data],
           });
         } else {
           console.error('알 수 없는 오류');
           setProductListPageState({
+            ...productListPageState,
             state: 'danger',
             text: '오류 : 알 수 없는 오류가 발생했습니다.',
-            data: '',
           });
         }
       })
       .catch((err) => {
         console.error(err);
       });
+    // eslint-disable-next-line
   }, [frmData]);
 
   return (
@@ -61,7 +71,7 @@ const ProductListPage = () => {
         text={productListPageState.text}
       />
       <ProductList
-        html={productListPageState.data}
+        htmls={productListPageState.data}
         onPushTableLength={pushTableLength}
       />
     </>
